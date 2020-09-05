70e60815aed19a907aaee5ce6ce3ee74548915fb
seyoungb <se.young492@gmail.com>
2020-09-05 22:23:59 +0900

    Updates

---
+++ precache-manifest.4c040220fc75c6f624c88456867cf966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c5098328c8a9e3157f1cd5ec24497a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "74327e0d16f6aa21bc3e",
+    "url": "/movie_app_2020/static/css/main.d64ea4cf.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74327e0d16f6aa21bc3e",
+    "url": "/movie_app_2020/static/js/main.8fe09592.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
